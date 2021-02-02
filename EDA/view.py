a369ef9ec1c2fcb54f350ebc9fae8d78ce211e87
JuanMonte3 <74276420+JuanMonte3@users.noreply.github.com>
2021-02-02 15:59:48 -0500

    cambios en view

--- EDA/view.py
+++ EDA/view.py
@@ -3,3 +3,17 @@ def menu():
     print("2. Linear")
     print("3. ")
     print("4. salir")
+end = True
+while end :
+    menu()
+    option =int(input("Digite su opcion: "))
+    if option == 1:
+        print("hola")
+    elif option == 2:
+        print("chao")
+    elif option == 3:
+        print("como")
+    else:
+        end = False 
+
+    
