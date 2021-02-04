bf20297265020ca72f845f1cde7bf61cf701ee9b
Juan Sebastian Montealegre <juan.montealegre1@hotmail.com>
2021-02-04 09:29:22 -0500

    Constantorder

--- EDA/model.py
+++ EDA/model.py
@@ -1,7 +1,14 @@
 import time
 
 def constant_order():
-    return ("Hola")
+    start_time = time.time_ns()
+    n = 7000
+    answer = "Impar"
+    if n % 2 == 0 :
+        answer = "Par"
+    end_time = time.time_ns()
+    print("El tiempo es igual:", end_time - start_time)
+    return answer
 
 def linear_order():
     return ("hola")
